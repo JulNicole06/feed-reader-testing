ffda9da7cda30de72dc02910d296639b2671e6af
Julia Fuhrman <JulNicole@gmail.com>
2018-06-26 17:28:58 -0400

    feat: Add test for menu visibility

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -58,16 +58,28 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        var body = $('body');
+        var menuIcon = $('.menu-icon-link');
 
-         it('is hidden by default', function() {
-            expect(menuIcon.attr('class')).not.toContain('menu-hidden');
-         });
+        it('is hidden by default', function() {
+            expect(body.attr('class')).toContain('menu-hidden');
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+        it('is visible when clicked', function() {
+            //click once
+            menuIcon.trigger( "click" );
+            expect(body.attr('class')).not.toContain('menu-hidden');
+
+            //click twice
+            menuIcon.trigger( "click" );
+            expect(body.attr('class')).toContain('menu-hidden');
+        });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
